469df342a1f44afdbf655fa2dd66894c75724f7a
Siddharth Kundu <sk31395@gmail.com>
2023-03-08 14:27:15 +0530

    first smoke test.

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Printf("Hello Go URL Shortener")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hey Go URL Shortener !",
+		})
+	})
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
 }
